de6e524e3de4b6a927fbe17ed45c542e65ee9ea0
fahadkabali <kabalifahad@gmail.com>
2024-10-19 02:05:35 +0300

    update

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,1 +1,56 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+import { FontAwesome } from '@expo/vector-icons';
+
+
+function TabBarIcon(props: 
+    { name: React.ComponentProps<typeof FontAwesome>['name']; 
+        color: string }) {
+    return <FontAwesome size={24} {...props} style={{color:'#1BC464'}}/>
+}
+const TabsLayout = () => {
+    return (
+       <SafeAreaView edges={['top']} style={styles.safeArea}>
+         <Tabs
+            screenOptions={{
+                tabBarActiveTintColor:'#1BC464',
+                tabBarInactiveTintColor: 'gray',
+                tabBarLabelStyle: {
+                    fontSize: 16,
+                },
+                tabBarStyle: {
+                    height: 80,
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,   
+                    paddingTop: 10,
+                }
+            }}
+            >
+            <Tabs.Screen 
+                name="index" 
+                options={{
+                    title: "Shop",
+                    tabBarIcon(props){
+                        return <TabBarIcon {...props} name="shopping-cart" />
+                    },
+                    // headerShown: false,
+                }}
+            />
+            <Tabs.Screen name="orders" options={{
+                title: "Orders",
+                tabBarIcon(props){
+                    return <TabBarIcon {...props} name="list" />
+                }
+            }}/>       
+        </Tabs>
+       </SafeAreaView>
+    );
+};
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1
+    }
+})
+export default TabsLayout
--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const Home = () => {
-  return (
-    <View>
-      <Text> Home</Text>
-    </View>
-  )
-}
-
-export default Home
-
-const styles = StyleSheet.create({})
+import {
+    ActivityIndicator,
+    FlatList,
+    StyleSheet,
+    View,
+    Text,
+  } from 'react-native';
+  
+  import { PRODUCTS } from '../../../assets/products';
+  import { ProductListItem } from '../../components/product-list-item';
+  import { ListHeader } from '../../components/list-header';
+//   import { getProductsAndCategories } from '../../api/api';
+  
+  const Home = () => {
+  
+  
+    return (
+      <View>
+        <FlatList
+          data={PRODUCTS}
+          renderItem={({ item }) => <ProductListItem product={item} />}
+          keyExtractor={item => item.id.toString()}
+          numColumns={2}
+          ListHeaderComponent={<ListHeader categories={data.categories} />}
+          contentContainerStyle={styles.flatListContent}
+          columnWrapperStyle={styles.flatListColumn}
+          style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+        />
+      </View>
+    );
+  };
+  
+  export default Home;
+  
+  const styles = StyleSheet.create({
+    flatListContent: {
+      paddingBottom: 20,
+    },
+    flatListColumn: {
+      justifyContent: 'space-between',
+    },
+  });
