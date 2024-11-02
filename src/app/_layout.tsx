b6406c006d969a0787723e77b69f892cddd6b873
fahadkabali <kabalifahad@gmail.com>
2024-11-03 01:07:47 +0300

    initial commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,18 @@
 import { Stack } from "expo-router";
+import { ToastProvider } from 'react-native-toast-notifications'
+
 
 export default function RootLayout() {
     return( 
-        <Stack>
-            <Stack.Screen name="(shop)" options={{ headerShown: false, title: "Shop" }} />
-            <Stack.Screen name="categories" options={{ headerShown: false, title: "Categories" }} />
-            <Stack.Screen name="product" options={{ headerShown: false, title: "Product" }} />
-            <Stack.Screen name="cart" options={{ presentation: "modal", title: "Shopping cart" }} />
-            <Stack.Screen name="auth" options={{ headerShown: false}} />
-            
-        </Stack>
+        <ToastProvider>
+            <Stack>
+                <Stack.Screen name="(shop)" options={{ headerShown: false, title: "Shop" }} />
+                <Stack.Screen name="categories" options={{ headerShown: false, title: "Categories" }} />
+                <Stack.Screen name="product" options={{ headerShown: false, title: "Product" }} />
+                <Stack.Screen name="cart" options={{ presentation: "modal", title: "Shopping cart" }} />
+                <Stack.Screen name="auth" options={{ headerShown: false}} />
+                
+            </Stack>
+        </ToastProvider>
     )
 }
