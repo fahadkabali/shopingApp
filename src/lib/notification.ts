bbf1ca68346051086ae53f01ca28bcc6e838cdd4
fahadkabali <kabalifahad@gmail.com>
2024-11-10 20:22:36 +0300

    update

--- src/lib/notification.ts
+++ src/lib/notification.ts
@@ -3,3 +3,10 @@ import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import Constants from 'expo-constants';
 
+function handleRegistrationError(errorMessage: string) {
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
+  
+ 
+  
