320b1ab59279704a673f6b7a8980aef53cfd19ab
fahadkabali <kabalifahad@gmail.com>
2024-11-17 22:08:14 +0300

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,30 +1,19 @@
-import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SecureStore from 'expo-secure-store';
 import * as aesjs from 'aes-js';
 import 'react-native-get-random-values';
-import { Database } from '../types/database.types';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
-
-// As Expo's SecureStore does not support values larger than 2048
-// bytes, an AES-256 key is generated and stored in SecureStore, while
-// it is used to encrypt/decrypt values stored in AsyncStorage.
+const supabaseUrl = process.env.YOUR_REACT_NATIVE_SUPABASE_URL!
+const supabaseAnonKey = process.env.YOUR_REACT_NATIVE_SUPABASE_ANON_KEY!
 class LargeSecureStore {
   private async _encrypt(key: string, value: string) {
     const encryptionKey = crypto.getRandomValues(new Uint8Array(256 / 8));
 
-    const cipher = new aesjs.ModeOfOperation.ctr(
-      encryptionKey,
-      new aesjs.Counter(1)
-    );
+    const cipher = new aesjs.ModeOfOperation.ctr(encryptionKey, new aesjs.Counter(1));
     const encryptedBytes = cipher.encrypt(aesjs.utils.utf8.toBytes(value));
 
-    await SecureStore.setItemAsync(
-      key,
-      aesjs.utils.hex.fromBytes(encryptionKey)
-    );
+    await SecureStore.setItemAsync(key, aesjs.utils.hex.fromBytes(encryptionKey));
 
     return aesjs.utils.hex.fromBytes(encryptedBytes);
   }
@@ -35,10 +24,7 @@ class LargeSecureStore {
       return encryptionKeyHex;
     }
 
-    const cipher = new aesjs.ModeOfOperation.ctr(
-      aesjs.utils.hex.toBytes(encryptionKeyHex),
-      new aesjs.Counter(1)
-    );
+    const cipher = new aesjs.ModeOfOperation.ctr(aesjs.utils.hex.toBytes(encryptionKeyHex), new aesjs.Counter(1));
     const decryptedBytes = cipher.decrypt(aesjs.utils.hex.toBytes(value));
 
     return aesjs.utils.utf8.fromBytes(decryptedBytes);
@@ -46,9 +32,7 @@ class LargeSecureStore {
 
   async getItem(key: string) {
     const encrypted = await AsyncStorage.getItem(key);
-    if (!encrypted) {
-      return encrypted;
-    }
+    if (!encrypted) { return encrypted; }
 
     return await this._decrypt(key, encrypted);
   }
@@ -65,7 +49,9 @@ class LargeSecureStore {
   }
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: new LargeSecureStore(),
     autoRefreshToken: true,
