6c4e9b11cdd07be4821d045a3f5cafe55e3e8271
fahadkabali <kabalifahad@gmail.com>
2024-12-18 16:03:31 +0300

    order component update

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,6 +1,8 @@
 import {    Stack } from "expo-router";
 import * as React from 'react';
+import { useOrderUpdateSubscription } from "../../../api/subscriptions";
 export default function OrderLayout() {
+    useOrderUpdateSubscription()
     return (
     <Stack>
         <Stack.Screen 
